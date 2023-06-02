13a25434932ad7d50f663f57809516f3e3c11d64
pro-3147-smart <pro-3147@telesmart.ir>
2023-06-02 10:51:58 +0330

    add function sms

---
+++ app/Http/Controllers/API/SmsController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Http\Controllers\API;
+use App\Models\Log;
+use App\Models\user\UserVerify;
+use App\Models\VerifyCode;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use Illuminate\Http\JsonResponse;
+
+class SmsController extends BaseController
+{
+    public function ($number, $content)
+    {
+    
+        ini_set("soap.wsdl_cache_enabled", "0");
+        $sms_client = new \SoapClient('http://payamak-service.ir/SendService.svc?wsdl', array('encoding' => 'UTF-8'));
+
+        try {
+            $parameters['userName'] = "";
+            $parameters['password'] = '';
+            $parameters['fromNumber'] = "10000100000";
+            $parameters['toNumbers'] = array($number);
+            $parameters['messageContent'] = $content;
+            $parameters['recId'] = &$recId;
+            $parameters['status'] = &$status;
+            $res = $sms_client->SendSMS($parameters)->SendSMSResult;
+            return true;
+        } catch (Exception $e) {
+            echo 'Caught exception: ', $e->getMessage(), "\n";
+        }
+
+
+    }
+
+  
+}
