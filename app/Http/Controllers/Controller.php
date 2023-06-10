716a9e0e841f2aa45e4460783c46d83ce86808fa
pro-3157 <pro-3157@telesmart.ir>
2023-06-10 08:08:48 +0330

    Added swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,20 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+ /**
+ * @OA\Schema(
+ *  schema="Result",
+ *  title="Sample schema for using references",
+ * 	@OA\Property(
+ * 		property="status",
+ * 		type="string"
+ * 	),
+ * 	@OA\Property(
+ * 		property="error",
+ * 		type="string"
+ * 	)
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
