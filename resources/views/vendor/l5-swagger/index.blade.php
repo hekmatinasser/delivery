de5338c6f2a4bf8085af062fb152c0aa95c1cfb6
pro-3157 <pro-3157@telesmart.ir>
2023-06-12 11:41:06 +0330

    Update

--- resources/views/vendor/l5-swagger/index.blade.php
+++ resources/views/vendor/l5-swagger/index.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-@if (env('APP_ENV') == 'local')
+@if (env('APP_ENV') != 'production')
 <head>
     <meta charset="UTF-8">
     <title>{{config('l5-swagger.documentations.'.$documentation.'.api.title')}}</title>
